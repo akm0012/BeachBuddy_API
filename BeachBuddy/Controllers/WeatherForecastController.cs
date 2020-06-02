20f19ff2c9fc103e2b681a24bf417e0b4af20aac
akm0012 <akm0012@auburn.edu>
2020-06-01 20:19:09 -0400

    Added some basic auth

--- BeachBuddy/Controllers/WeatherForecastController.cs
+++ BeachBuddy/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace BeachBuddy.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/weather")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
--- BeachBuddy/Startup.cs
+++ BeachBuddy/Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,9 +18,12 @@ namespace BeachBuddy
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private IHostEnvironment CurrentEnvironment { get; set; }
+
+        public Startup(IConfiguration configuration, IHostEnvironment hostEnvironment)
         {
             Configuration = configuration;
+            CurrentEnvironment = hostEnvironment;
         }
 
         public IConfiguration Configuration { get; }
@@ -26,15 +32,66 @@ namespace BeachBuddy
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Library used to map objects to DTOs
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            // services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
+            //
+            // services.AddDbContext<BeachBuddyContext>(options =>
+            // {
+            //
+            //     if (CurrentEnvironment.IsDevelopment())
+            //     {
+            //         // Use local Docker DB
+            //         options.UseSqlServer(Configuration.GetConnectionString("CourseLibraryDB"));
+            //     }
+            //     else
+            //     {
+            //         // In Prod use the Docker-Compose database
+            //         var connection = @"Server=db;Database=CourseLibraryDB;User=sa;Password=abcABC123;";
+            //         options.UseSqlServer(connection);
+            //     }
+            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        // Todo: This is where you could log this stuff
+                        await context.Response.WriteAsync(
+                            "Red Flag! Get out of the ocean as there was a server error!");
+                    });
+                });
+            }
+
+            // Check if the secret access header is in the request
+            app.Use(async (context, next) =>
+                {
+                    if (!context.Request.Headers.ContainsKey("AppToken") ||
+                        context.Request.Headers["AppToken"] != APIKeys.AppSecretHeader)
+                    {
+                        logger.LogWarning("Token was not found! Intruder alert!");
+                        context.Response.StatusCode = 401;
+                        await context.Response.WriteAsync("No seagulls allowed. We don't know who you are.");
+                    }
+                    else
+                    {
+                        await next.Invoke();
+                    }
+                }
+            );
 
             app.UseHttpsRedirection();
 
