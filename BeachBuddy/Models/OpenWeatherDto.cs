9630140119394fc5ce2ff0b54a7ec4bd7f544536
akm0012 <akm0012@auburn.edu>
2020-06-07 17:10:31 -0400

    Added the weather DTOs

--- BeachBuddy/Models/OpenWeatherDto.cs
+++ BeachBuddy/Models/OpenWeatherDto.cs
@@ -11,10 +11,12 @@ namespace BeachBuddy.Models
         public double lon { get; set; }
 
         public Current current { get; set; }
-        
+
         public IEnumerable<MinuteForecast> minutely { get; set; }
-        
+
         public IEnumerable<HourlyWeatherForecast> hourly { get; set; }
+        
+        public IEnumerable<DailyForecast> daily { get; set; }
     }
 
     public class Current
@@ -34,44 +36,94 @@ namespace BeachBuddy.Models
 
         public double uvi { get; set; }
 
-        public double clouds { get; set; }
+        public int clouds { get; set; }
 
         public double wind_speed { get; set; }
 
         public double wind_gust { get; set; }
-        
-        public IEnumerable weather { get; set; }
+
+        public IEnumerable<Weather> weather { get; set; }
     }
 
     public class Weather
     {
         public string main { get; set; }
-        
+
         public string description { get; set; }
-        
+
         public string icon { get; set; }
     }
 
     public class MinuteForecast
     {
         public long dt { get; set; }
-        
+
         public double precipitation { get; set; }
     }
-    
-    public class HourlyWeatherForecast {
+
+    public class HourlyWeatherForecast
+    {
         public long dt { get; set; }
-        
+
         public double temp { get; set; }
 
         public double feels_like { get; set; }
-        
-        public double humidity { get; set; }
+
+        public int humidity { get; set; }
 
         public double clouds { get; set; }
-        
+
         public double wind_speed { get; set; }
 
         public IEnumerable weather { get; set; }
+    }
+
+    public class DailyForecast
+    {
+        public long dt { get; set; }
+
+        public long sunrise { get; set; }
+
+        public long sunset { get; set; }
+
+        public DailyTempForecast temp { get; set; }
+        
+        public FeelsLikeTempForecast feels_like { get; set; }
+        
+        public int humidity { get; set; }
+        
+        public double wind_speed { get; set; }
+        
+        public IEnumerable weather { get; set; }
+        
+        public double rain { get; set; }
+        
+        public double uvi { get; set; }
+    }
+
+    public class DailyTempForecast
+    {
+        public double day { get; set; }
+
+        public double min { get; set; }
+
+        public double max { get; set; }
+
+        public double night { get; set; }
+
+        public double eve { get; set; }
+
+        public double morn { get; set; }
+    }
+    
+    public class FeelsLikeTempForecast
+    {
+        public double day { get; set; }
+
+        public double night { get; set; }
+
+        public double eve { get; set; }
+
+        public double morn { get; set; }
     }
 }
